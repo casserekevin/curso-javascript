0581b5744a83a4316d19479a4ad92af2ee502dcb
casserekevin <kevin_cassere13@hotmail.com>
2019-03-11 18:36:29 -0300

    Curso Javascript - Aula 5

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-document.write("Curso Javascript - Aula 4<br>")
+document.write("Curso Javascript - Aula 5<br>")
 
 var canal = `Cursos`
 var curso = `Curso de Javascript`
 const separador = ` - ` 
 
-alert(`${canal + separador + curso}`)
+var nome = prompt("Digite se nome:", "nome aqui")
+
+alert(nome);
