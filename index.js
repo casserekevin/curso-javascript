69b82770a12f61e2abe079fb7f3d9db8117954c6
casserekevin <kevin_cassere13@hotmail.com>
2019-03-11 13:22:37 -0300

    Curso Javascript - Aula 1

---
+++ index.js
@@ -0,0 +1,10 @@
+document.write("Curso Javascript - Aula 1<br>")
+
+document.write("texto<br>")
+document.write(`${10}<br>`)
+document.write(`${10 + 2}<br>`)
+
+var multilineString = `<div style="color:#f00">${10} ${10 + 2} A string <br>on multiple lines</div>`
+
+document.write(multilineString)
+document.write("Curso de<br>Javascript")
