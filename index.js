004290875c987cce484747194c7c7995a76fba6d
casserekevin <kevin_cassere13@hotmail.com>
2019-03-12 00:41:06 -0300

    Curso Javascript - Aula 7

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-document.write("Curso Javascript - Aula 6<br>")
+document.write("Curso Javascript - Aula 7<br>")
 
-var nome = prompt("Digite se nome", "nome aqui")
-var res = confirm(`${nome} dejesa continuar?`)
-alert(res)
+var v1, v2, txt1, txt2
+
+v1 = 10
+v2 = 10
+
+txt1 = "Cursos"
+txt2 = "Curso de Javascript"
+
+if(v1 == v2){
+	document.write(txt1)
+	alert(txt1)
+}
+else{
+	document.write(txt2)
+	alert(txt2)
+}
