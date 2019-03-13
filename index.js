c1b344869b9c3cef39f2f48169e6eea6ebffb6b7
casserekevin <kevin_cassere13@hotmail.com>
2019-03-13 15:43:00 -0300

    Curso Javascript - Aula 13

--- index.js
+++ index.js
@@ -1,44 +1,13 @@
-document.write("Curso Javascript - Aula 12<br><br>")
+document.write("Curso Javascript - Aula 13<br><br>")
 
-var pos, msg, pontos
+// let array = new Array()
+let itens = ["Faca", "Pederneira", "Corda", "Lanterna", "Luvas", "Chave de fenda", "Arame"]
+let mochila = []
 
-pontos = 500
+mochila.push(5)
+mochila.push(0)
+mochila.push(3)
 
-if(pontos >= 1000){
-	pos = 1
-}
-else if(pontos >= 800){
-	pos = 2
-}
-else if(pontos >= 600){
-	pos = 3
-}
-else{
-	pos = 0
-}
-
-switch(pos){
-	case 1:
-		msg = "Primeiro Lugar<br>Medalha de Ouro"
-		break
-	case 2:
-		msg = "Segundo Lugar<br>Medalha de Prata"
-		break
-	case 3:
-		msg = "Terceiro Lugar<br>Medalha de Bronze"
-		break
-	default:
-		msg = "Não foi ao pódio<br>Não obteve medalha"	
-}
-
-switch(pos){
-	case 1:
-	case 2:
-	case 3:
-		msg += "<br>Subiu ao pódio"
-		break
-	default:
-		msg += "<br>Não obtenção colocação"	
-}
-
-document.write(msg)
+document.write(`${itens[mochila[0]]}<br>`)
+document.write(`${itens[mochila[1]]}<br>`)
+document.write(`${itens[mochila[2]]}<br>`)
