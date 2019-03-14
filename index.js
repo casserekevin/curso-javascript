6b8a48986dbc7814933f147153934b8d8d7fc7ef
casserekevin <kevin_cassere13@hotmail.com>
2019-03-14 17:09:30 -0300

    Curso Javascript - Aula 15

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
-document.write("Curso Javascript - Aula 14<br><br>")
+document.write("Curso Javascript - Aula 15<br><br>")
 
 // let array = new Array()
 let mochila = []
+let mochila1 = []
+let mochila2 = []
 
-mochila.push("Faca")
-mochila.push("Corda")
-mochila.push("Pederneira")
-mochila.push("Chave")
-mochila.push("Lanterna")
-mochila.push("Pedra")
+mochila1.push("Faca")
+mochila1.push("Corda")
+mochila1.push("Pederneira")
+mochila1.push("Chave")
+mochila1.push("Lanterna")
+mochila1.push("Pedra")
+mochila1.push("Mouse")
 
-mochila.splice(1,1)
-mochila.splice(2,1)
+mochila2.push("Arame")
+mochila2.push("Luvas")
+mochila2.push("Joelheira")
+mochila2.push("Meias")
+mochila2.push("Moeda")
+mochila2.push("Canivete")
 
-document.write(`${mochila[0]}<br>`)
-document.write(`${mochila[1]}<br>`)
-document.write(`${mochila[2]}<br>`)
-document.write(`${mochila[3]}<br>`)
-document.write(`${mochila[4]}<br>`)
-document.write(`${mochila[5]}<br>`)
+mochila1.pop()
+
+mochila = mochila1.concat(mochila2)
+
+document.write(`${mochila.join('<br>')}<br>`)
+
+document.write(`Tamanho da mochila 1: ${mochila1.length}<br>`)
+document.write(`Tamanho da mochila 2: ${mochila2.length}<br>`)
+document.write(`Tamanho da mochila: ${mochila.length}<br>`)
+
