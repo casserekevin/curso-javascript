e68c57fa8ce32e3f01eddc570daf489b495f7622
casserekevin <kevin_cassere13@hotmail.com>
2019-03-14 20:50:07 -0300

    Curso Javascript - Aula 17

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
-document.write("Curso Javascript - Aula 16<br><br>")
+document.write("Curso Javascript - Aula 17<br><br>")
 
-let mochila = []
+let num = 10
 
-let item1 = ["Corda", 2]
-let item2 = ["Faca", 1]
-let item3 = ["Cura", 5]
-let item4 = ["Prego", 35]
+num *= -1
 
-mochila.push(item1)
-mochila.push(item2)
-mochila.push(item3)
-mochila.push(item4)
-
-document.write(`${mochila[0][0]} - ${mochila[0][1]}<br>`)
-document.write(`${mochila[1][0]} - ${mochila[1][1]}<br>`)
-document.write(`${mochila[2][0]} - ${mochila[2][1]}<br>`)
-document.write(`${mochila[3][0]} - ${mochila[3][1]}<br>`)
+document.write(-num)
+document.write("<br>")
+document.write(num) 
